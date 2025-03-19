9cbd49cbed766a125de4c9f9ca9cf0c2648b436d
AshokMahes07 <ashokmahes07@gmail.com>
2025-03-19 18:16:20 +0530

    Logo has updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,32 @@
+import Image from "next/image";
 import React, { ReactNode } from "react";
 import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-white text-black border-b border-gray-300 fixed top-0 left-0 right-0 z-10 h-16 flex items-center px-4">
       <div className="container mx-auto flex justify-between">
-        <div className="text-white text-lg font-bold">Gallery App</div>
-        <div className="space-x-4">
-          <Link href="/ViewGallery" className="text-white hover:underline">
+        <div className="flex items-center h-screen">
+          <Image
+            className=""
+            src="/logo.png"
+            alt="PROINSPEC"
+            width={180}
+            height={38}
+            priority
+          />
+        </div>
+        <div className="flex items-center h-screen space-x-4">
+          <Link href="/ViewGallery" className="hover:underline">
             View Gallery
           </Link>
-          <Link href="/PdfReport" className="text-white hover:underline">
+          <Link href="/PdfReport" className="hover:underline">
             PDF Report
           </Link>
-          <Link href="/ChangesOnly" className="text-white hover:underline">
+          <Link href="/ChangesOnly" className="hover:underline">
             Changes Only
           </Link>
-          <Link href="/ActionOnly" className="text-white hover:underline">
+          <Link href="/ActionOnly" className="hover:underline">
             Action Only
           </Link>
           <Link href="/ViewInternal" className="text-white hover:underline">
